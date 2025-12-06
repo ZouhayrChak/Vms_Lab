aaaf5afa5582975ff5bf56cfe2934596e66c53ec
Zouhayr Chakroun <Zouhayr.chak24@gmail.com>
2025-12-06 15:34:42 +0100

    good

--- Microservice/app/VmsApi.py
+++ Microservice/app/VmsApi.py
@@ -14,7 +14,7 @@ class Vm(Api):
         self.exposeApi()
 
     def exposeApi(self):
-        @self.app.route(VM_URL+"/",methods=["POST"])
+        @self.app.route(VM_URL,methods=["POST"])
         def createVm():
             vm_data = request.get_json()
             print(vm_data)
@@ -23,7 +23,7 @@ class Vm(Api):
 
             natBridge.createNatBridge(nb_data)
             try:
-                subprocess.check_call(f"docker run --name={vm_data['nameVm']} --net=sbr{vm_data['idSb']} --ip={vm_data['ipVm']} --rm --privileged -d docker:dind".split())
+                subprocess.check_call(f"docker run --name={vm_data['nameVm']} --net=sbr{vm_data['idSb']} --ip={vm_data['ipVm']} --rm --privileged -d ubuntu".split())                
                 subprocess.check_call(f"docker network connect nbr{vm_data['idSb']}{vm_data['idNb']} {vm_data['nameVm']}".split())
                 response = {"message":"vm created","success":True},201
             except Exception:
@@ -33,12 +33,12 @@ class Vm(Api):
                 response = {"message":"could not create vm","success":False},404
             return response
 
-        @self.app.route(VM_URL+"/",methods=["DELETE"])
+        @self.app.route(VM_URL,methods=["DELETE"])
         def deleteVm():
             try:
                 data = request.get_json()
                 subprocess.check_call(f"docker stop {data['nameVm']}".split())
-                subprocess.check_call(f"docker network rm nbr{data['idNb']}".split())
+                subprocess.check_call(f"docker network rm nbr{vm_data['idSb']}{data['idNb']}".split())
 
 
                 response = {"message":"vm deleted","success":True},200
